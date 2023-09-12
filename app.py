716bda32f93c013313d6423c70c3c9b498be6a67
berkaygemici <e210506002@stud.tau.edu.tr>
2023-09-12 18:07:47 +0300

    Changed the Sign Up protocol.

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, redirect, request
 import sqlite3
 
 app = Flask(__name__)
-conn = sqlite3.connect('database.db',check_same_thread=False)
+#conn = sqlite3.connect('database.db',check_same_thread=False)
+try:
+    conn = sqlite3.connect('database.db',check_same_thread=False)
+    print("Connection succesfull")
+except sqlite3.Error as e:
+    print(f"Connection error:{e}")
 cursor = conn.cursor()
 
 # PAGE ROUTES
@@ -35,10 +40,10 @@ def faq():
 def signup_handler():
     email = request.form['email']
     password = request.form['password']
-    try:
-        cursor.execute("INSERT INTO users(user_mail,user_pass) VALUES(?,?)", (email, password))
-        conn.commit()
-    except sqlite3.OperationalError:
+    if is_email_used(email):
+        message = "This e-mail is already in use. Please choose another mail or log in to your account!"
+        return render_template('pages/signup.html',message=message)
+    else:
         cursor.execute("INSERT INTO users(user_mail,user_pass) VALUES(?,?)", (email, password))
         conn.commit()
     return redirect("/login")
@@ -48,11 +53,15 @@ def login_handler():
     password = request.form["password"]
     cursor.execute("SELECT user_id FROM users where user_mail =? AND user_pass =?",(email,password))
     search = cursor.fetchone()
-    if search:
+    #if search:
 
     return redirect("/")
-# FUNCTIONAL GATEWAYS...
 
+# FUNCTIONAL GATEWAYS...
+def is_email_used(email):
+    cursor.execute("SELECT user_id FROM users WHERE user_mail=?",(email,))
+    already_registered = cursor.fetchone()
+    return already_registered is not None
 
 if __name__ == '__main__':
     app.run(debug=True)
