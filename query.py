0d2df3ea65f06879e42611ff8f3cf271943d7c28
surkanfuren <surenfurkcan@gmail.com>
2023-09-20 01:05:10 +0300

    Code verification not fixed.

--- query.py
+++ query.py
@@ -1,35 +1,13 @@
 import sqlite3
 import hashlib
-
-conn = sqlite3.connect('database.db', check_same_thread=False)
-cursor = conn.cursor()
-
-def hash_password(password):
-    sha256 = hashlib.sha256()
-    sha256.update(password.encode('utf-8'))
-    hashed_password = sha256.hexdigest()
-    return hashed_password
+import pyotp
 
 
-email = "furkan@mail.com"
-password_first = "furkan"
-password = hash_password(password_first)
+secret_key = pyotp.random_base32()
+hotp = pyotp.HOTP(secret_key)
+verification_code = hotp.at(0)
+print(verification_code)
 
-cursor.execute("SELECT * FROM users WHERE user_mail =? AND user_pass =?", (email, password))
-user = cursor.fetchone()
-
-print(user[1].split("@")[0])
-print(user[3])
-
-cursor.execute("SELECT product_name FROM products WHERE user_id = ?", (11,))
-payload = cursor.fetchall()
-
-for i in payload:
-    print(i[0])
-
-
-cursor.execute("SELECT * FROM users")
-getit = cursor.fetchall()
-
-for i in getit:
-    print(i)
+input_code = input()
+is_verified = hotp.verify(input_code, 0)
+print(is_verified)
